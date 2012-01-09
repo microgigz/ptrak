988d0f4664ef2ca9e1adf3b6b5c989ad16a380f3
Rashid Minhas <rashid.minhas.nxb@gmail.com>
2012-01-09 10:26:45 +0500

    abc

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,27 +1,84 @@
 class ProjectsController < ApplicationController
-  def new
-    @project=Project.new
-    
+  layout 'project'
+  # GET /projects
+  # GET /projects.xml
+  def index
+    @projects = Project.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @projects }
+    end
   end
 
+  # GET /projects/1
+  # GET /projects/1.xml
   def show
-    @projects=current_user.projects
+    @project = Project.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @project }
+    end
   end
 
+  # GET /projects/new
+  # GET /projects/new.xml
+  def new
+    @project = Project.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @project }
+    end
+  end
+
+  # GET /projects/1/edit
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  # POST /projects
+  # POST /projects.xml
   def create
     @project = Project.new(params[:project])
-    @project.user_id = current_user.id
-    @project.status= "in_progress"
-    @project.curr_project=true
-    Project.all.each do |p|
-      next if (p.id == @project.id)
-      p.curr_project= false
-    end
-    @project.save
+
     respond_to do |format|
-      format.html{ redirect_to project_path @project.id }
+      if @project.save
+        format.html { redirect_to(@project, :notice => 'Project was successfully created.') }
+        format.xml  { render :xml => @project, :status => :created, :location => @project }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @project.errors, :status => :unprocessable_entity }
+      end
     end
-    
-#    redirect_to(:action => "usecase" , :controller => "stories") if @project.save
+  end
+
+  # PUT /projects/1
+  # PUT /projects/1.xml
+  def update
+    @project = Project.find(params[:id])
+
+    respond_to do |format|
+      if @project.update_attributes(params[:project])
+        format.html { redirect_to(@project, :notice => 'Project was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @project.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /projects/1
+  # DELETE /projects/1.xml
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(projects_url) }
+      format.xml  { head :ok }
+    end
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,2 @@
 class Project < ActiveRecord::Base
-  belongs_to :user
-  has_many :tasks
-
-  after_create  do |record|
-        user = record.user; user.projects.update_all("curr_project = '0 '", ['id <> ?', record.id])
-  end
-
 end
