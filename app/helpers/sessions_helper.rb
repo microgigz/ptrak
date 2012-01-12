770b437281447334e951e3922753fb2487219ed8
Muhammad Abid <abid@nx00068.nextbridge.org>
2012-01-12 12:03:54 +0500

    devise fixed

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,25 +1,2 @@
 module SessionsHelper
-
-  def signedin?
-    if session[:id] == nil
-      return true
-    else
-      return false
-    end
-  end
-  def currentuser
-
-  end
-  def signout
-    
-  end
-  def deny_access
-    #store_loction
-    redirect_to signin_path, :notice => "Please Sign in to access the page"
-  end
-  
-#  def current_user
-#      @current_user ||= ((session[:user_id] && User.find_by_id(session[:user_id])) || 0)
-#
-#  end
 end
